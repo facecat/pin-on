62c543217499272efc6de1fb0bcb719c8353566c
facecat <facecat10002gmail.com>
2016-01-12 21:22:29 +0800

    Initial Commit

---
+++ pin-on/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pin-on
+//
+//  Created by 邱国邦 on 16/1/12.
+//  Copyright © 2016年 FaceCat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
