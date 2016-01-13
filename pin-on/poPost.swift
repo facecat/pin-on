8bc7439d887fb3bbd963a813f1ae9aee557db040
facecat <facecat10002gmail.com>
2016-01-14 07:40:54 +0800

    Add class poPost

---
+++ pin-on/poPost.swift
@@ -0,0 +1,48 @@
+//
+//  poPost.swift
+//  pin-on
+//
+//  Created by 邱国邦 on 16/1/14.
+//  Copyright © 2016年 FaceCat. All rights reserved.
+//
+
+import Foundation
+
+class poPost {
+    
+    private var _userName: String!
+    private var _userImg: String!
+    private var _postImg: String!
+    private var _likes: String!
+    private var _postTime: String!
+    
+    var userName: String {
+        return _userName
+    }
+    
+    var userImg: String {
+        return _userImg
+    }
+    
+    var postImg: String {
+        return _postImg
+    }
+    
+    var likes: String {
+        return _likes
+    }
+    
+    var postTime: String {
+        return _postTime
+    }
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
